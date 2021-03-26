54414146fed7e37e4e296095d5685358faab2516
Michael Levitin <michael9levi@gmail.com>
2021-03-26 20:59:04 +0300

    Parametrisation of context (number in array for ArrayStorage vs uiid(?) for MapStorage)

--- src/main/java/ru/javawebinar/webapp/storage/AbstractStorage.java
+++ src/main/java/ru/javawebinar/webapp/storage/AbstractStorage.java
@@ -1,13 +1,14 @@
 package ru.javawebinar.webapp.storage;
 
 import ru.javawebinar.webapp.WebAppException;
-import ru.javawebinar.webapp.model.ContactType;
 import ru.javawebinar.webapp.model.Resume;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.logging.Logger;
 
-abstract public class AbstractStorage implements IStorrage {
+abstract public class AbstractStorage<C> implements IStorrage {
   protected final Logger logger = Logger.getLogger(getClass().getName());
   // if it's static => mistake
 
@@ -19,47 +20,52 @@ abstract public class AbstractStorage implements IStorrage {
 
   protected abstract void doClear();
 
-  protected abstract boolean exist(String uiid);
+  protected abstract C getContext(String uiid); // Returns generic C
+  protected abstract boolean exist(C context);
 
   @Override
   public void save(Resume r) {
     logger.info("Saving resume with Uiid = " + r.getUuid());
-    if (exist(r.getUuid()))
+    C ctx = getContext(r);
+    if (exist(ctx))
       throw new WebAppException("Resume" + r.getUuid() + "already exists");
-    doSave(r);
+    doSave(ctx, r);
   }
 
-  protected abstract void doSave(Resume r);
+  protected abstract void doSave(C ctx, Resume r);
 
   @Override
   public void update(Resume r) {
     logger.info("Updated resume with uiid = " + r.getUuid());
-    if (!exist(r.getUuid()))
+    C ctx = getContext(r);
+    if (!exist(ctx))
       throw new WebAppException("Resume with uiid = " + r.getUuid() + "doesn't exists");
-    doUpdate(r);
+    doUpdate(ctx, r);
   }
 
-  protected abstract void doUpdate(Resume r);
+  protected abstract void doUpdate(C ctx, Resume r);
 
   @Override
   public Resume load(String uuid) {
     logger.info("Loading resume with uiid = " + uuid);
-    if (!exist(uuid))
-      throw new WebAppException("Resume with uiid = " + uuid + "doesn't exists");
-    return doLoad(uuid);
+    C ctx = getContext(uuid);
+    if (!exist(ctx))
+      throw new WebAppException("Resume with uiid = " + uuid + " doesn't exists");
+    return doLoad(ctx);
   }
 
-  protected abstract Resume doLoad(String uuid);
+  protected abstract Resume doLoad(C ctx);
 
   @Override
   public void delete(String uuid) {
     logger.info("Deleting resume with uiid = " + uuid);
-    if (!exist(uuid))
-      throw new WebAppException("Resume with uiid = " + uuid + "doesn't exists");
-    doDelete(uuid);
+    C ctx = getContext(uuid);
+    if (!exist(ctx))
+      throw new WebAppException("Resume with uiid = " + uuid + " doesn't exists");
+    doDelete(ctx);
   }
 
-  protected abstract void doDelete(String uuid);
+  protected abstract void doDelete(C idx);
 
   @Override
   public Collection<Resume> getAllSorted() {
@@ -79,7 +85,7 @@ abstract public class AbstractStorage implements IStorrage {
     });
     return list;*/
 //    -------------------------
-//      return o1.getUuid().compareTo(o2.getUuid());//        return   o1.getContacts(ContactType.MAIL).compareTo(o2.getContacts(ContactType.MAIL));
+//        return   o1.getContacts(ContactType.MAIL).compareTo(o2.getContacts(ContactType.MAIL));
 //        Cannot do like this - could be NPE, cause ContactType can be null
 
 //    return Collections.singletonList(new Resume()); return collection of one
@@ -89,6 +95,9 @@ abstract public class AbstractStorage implements IStorrage {
 
   public abstract int size();
 
+  private C getContext(Resume r) {
+    return getContext(r.getUuid());
+  }
 }
 
 
